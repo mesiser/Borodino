39720880cbf8706ca2cd293b11db976bd3c045da
Alexander <a.pride.xxi@tutanota.com>
2022-05-04 16:40:15 +0300

    initial commit

---
+++ Borodino/GalleryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Borodino
+//
+//  Created by Александр Прайд on 04.05.2022.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
